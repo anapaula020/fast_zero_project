968aa0ee915e08c6af7dcf854f15bfbea18f7af9
Ana Paula Gomes Miranda <paulagomes@unitins.br>
2025-07-22 10:01:22 -0300

    Implementando rotas CRUD

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -3,3 +3,18 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: str
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: str
